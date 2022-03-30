20b0a976c7a90a7ea55e80e0bc6ebfb0582b110b
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-30 17:45:30 +0200

    Add end-tag for filter.

--- pkg/southbound/get.go
+++ pkg/southbound/get.go
@@ -86,7 +86,8 @@ func GetConfig(paths []*gnmi.Path, format string, reqType gnmi.GetRequest_DataTy
 	for _, path := range paths {
 		for index, elem := range path.Elem {
 			if index == 0 {
-				cmd += "<filter type=\"subtree\">"
+				cmd += "<filter>" // type=\"subtree\">"
+				endOfCmd = "</filter>"
 			}
 			cmd += fmt.Sprintf("<%s", elem.Name)
 			endOfCmd = fmt.Sprintf("</%s>", elem.Name) + endOfCmd
