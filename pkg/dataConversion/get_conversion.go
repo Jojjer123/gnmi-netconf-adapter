67f325eedbf3f1e895d95ce1149b2d407fb023b2
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-05-16 15:23:02 +0200

    Log reply on WaitForFunc failed error.

--- pkg/dataConversion/get_conversion.go
+++ pkg/dataConversion/get_conversion.go
@@ -31,6 +31,7 @@ func ConvertAndSendReq(req *gnmi.GetRequest) *gnmi.GetResponse {
 
 	// If southbound fails to get config, return empty response
 	if err != nil {
+		log.Infof("The reply was: %v\n", reply)
 		log.Errorf("Failed to get response: %v\n", err)
 
 		notifications := make([]*gnmi.Notification, 1)
